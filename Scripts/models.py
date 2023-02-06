708571ffb99409356cefbac451b1d8fc2378cf59
HossamElDin AbdElGhani ElKordi <haelkordi@ejadaalex.local>
2023-02-06 17:07:06 +0200

    Removing Models

--- Scripts/models.py
+++ Scripts/models.py
@@ -1,8 +1,8 @@
 import torch
 from torch import nn
+from transformers import BertTokenizer
 from torch.nn import CrossEntropyLoss, LayerNorm
 from transformers.modeling_utils import PreTrainedModel
-from transformers import BertTokenizer, BertForMaskedLM
 from transformers.models.bert.modeling_bert import ACT2FN
 from Scripts.custom_criterion import CustomAdaptiveLogSoftmax
 from transformers.tokenization_utils import PreTrainedTokenizer
@@ -39,37 +39,28 @@ class TabFormerHierarchicalLM(PreTrainedModel):
 
 
 class TabFormerBertLM:
-    def __init__(self, special_tokens, vocab, field_ce=False, flatten=False, ncols=None, field_hidden_size=768):
+    def __init__(self, special_tokens, vocab, ncols=None, field_hidden_size=768):
 
         self.ncols = ncols
         self.vocab = vocab
         vocab_file = self.vocab.filename
-        hidden_size = field_hidden_size if flatten else (field_hidden_size * self.ncols)
+        hidden_size =  field_hidden_size * self.ncols
 
         self.config = TabFormerBertConfig(vocab_size=len(self.vocab),
                                           ncols=self.ncols,
                                           hidden_size=hidden_size,
                                           field_hidden_size=field_hidden_size,
-                                          flatten=flatten,
+                                          flatten=False,
                                           num_attention_heads=self.ncols)
 
         self.tokenizer = BertTokenizer(vocab_file,
                                        do_lower_case=False,
                                        **special_tokens)
-        self.model = self.get_model(field_ce, flatten)
+        self.model = self.get_model()
 
-    def get_model(self, field_ce, flatten):
-
-        if flatten and not field_ce:
-            # flattened vanilla BERT
-            model = BertForMaskedLM(self.config)
-        elif flatten and field_ce:
-            # flattened field CE BERT
-            model = TabFormerBertForMaskedLM(self.config, self.vocab)
-        else:
-            # hierarchical field CE BERT
-            model = TabFormerHierarchicalLM(self.config, self.vocab)
-
+    def get_model(self):
+        # hierarchical field CE BERT
+        model = TabFormerHierarchicalLM(self.config, self.vocab)
         return model
 
 
