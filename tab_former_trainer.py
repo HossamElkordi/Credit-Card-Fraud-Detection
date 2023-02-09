e0e4b1515fe950d6dc50b79faebb4603702a1924
HossamElDin AbdElGhani ElKordi <haelkordi@ejadaalex.local>
2023-02-09 15:05:10 +0200

    changing file path

--- tab_former_trainer.py
+++ tab_former_trainer.py
@@ -8,7 +8,7 @@ from Scripts.utils import random_split_dataset
 from Scripts.tab_former_dl import CustomTrainer
 from Scripts.tabformer_models import TabFormerBertLM
 from Scripts.datacollator import TransDataCollatorForLanguageModeling
-from transformers import DataCollatorForLanguageModeling, TrainingArguments, ProgressCallback
+from transformers import DataCollatorForLanguageModeling, TrainingArguments, ProgressCallback, PrinterCallback
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -38,6 +38,17 @@ def main(args):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
     
+    ids = {576,  102, 1429,  486, 1394, 1996, 1982, 1069,  341,  252, 1844,
+        557, 1514, 1585,  266, 1419, 1515, 1722, 1060,   98, 1911, 1278,
+       1739,   41,  759,  946, 1096,  185,  970, 1983,  764,  854,  257,
+       1129,  605, 1685,  923, 1271, 1572,   81,  499,  680, 1792,  395,
+        814, 1029,  996, 1680, 1399,  619,  624, 1539,  625, 1357,  262,
+       1324, 1766,  549, 1126,  138,  817, 1304,  987, 1423,  417, 1731,
+        683, 1681, 1378,  913, 1330, 1338,  220, 1211, 1532,  446,  504,
+        100, 1101, 1880,  320,  838, 1327, 1002,  780,  945, 1196, 1122,
+       1885, 1528,  413, 1248,  615, 1607,  190,  914,  897,  813, 1709,
+       1019, 1604, 1425, 1487, 1064}
+
     dataset = Data(
         data_dir=args.data_dir,
         model_dir=args.model_dir,
@@ -46,19 +57,21 @@ def main(args):
         nbins=args.nbins,
         return_labels=args.return_labels,
         skip_user=args.skip_user,
-        flatten=args.flatten
+        flatten=args.flatten,
+        ids=ids
     )
 
     vocab = dataset.vocab
     custom_special_tokens = vocab.get_special_tokens()
 
     # split dataset into train, val, test [0.7. 0.1, 0.2]
-    totalN = len(dataset)
+    totalN = len(dataset.data)
     trainN = int(0.6 * totalN)
     valtestN = totalN - trainN
     valN = int(valtestN * 0.5)
     testN = valtestN - valN
     lengths = [trainN, valN, testN]
+
     train_dataset = TransactionData(
         dataset.data[:lengths[0]],
         args.data_dir,
@@ -98,7 +111,7 @@ def main(args):
 
     model_path = os.path.join(args.model_dir, 'checkpoints')
     training_args = TrainingArguments(
-        output_dir='/content/kdrive/MyDrive/IBM_Dataset/checkpoints',
+        output_dir=model_path,
         num_train_epochs=args.epochs,
         save_steps=args.save_step,
         do_train=args.do_train,
@@ -117,6 +130,7 @@ def main(args):
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
     )
+    trainer.remove_callback(PrinterCallback)
     trainer.train()
 
 if __name__ == '__main__':
